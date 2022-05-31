644c178894a740ac2eb1f9152b73d83cc55a2bb7
amerkurev <a-merkurev@mail.ru>
2022-05-31 18:41:13 +0300

    add tests for the hash package

---
+++ internal/hash/hash_test.go
@@ -0,0 +1,34 @@
+package hash
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+func TestHasherError_Error(t *testing.T) {
+	err := errors.New("some error")
+	hErr := &HasherError{
+		Type: reflect.TypeOf(int64(1)),
+		Err:  err,
+	}
+	assert.Equal(t, hErr.Error(), "hash error for type int64: some error")
+	assert.True(t, errors.Is(hErr, err))
+	assert.True(t, errors.As(hErr, &err))
+}
+
+func TestMsgpackHasher_Hash(t *testing.T) {
+	h := &MsgpackHasher{}
+	k, err := h.Hash(map[string]struct{}{})
+	assert.Nil(t, err)
+	assert.Equal(t, k, "76be8b528d0075f7aae98d6fa57a6d3c83ae480a8469e668d7b0af968995ac71")
+
+	k, err = h.Hash(100)
+	assert.Nil(t, err)
+	assert.Equal(t, k, "18ac3e7343f016890c510e93f935261169d9e3f565436429830faf0934f4f8e4")
+
+	k, err = h.Hash("some key")
+	assert.Nil(t, err)
+	assert.Equal(t, k, "0dc44df765b1ef70e8b5069777b6cb177fdeef0cc977327b9e19e4a3dad24818")
+}
